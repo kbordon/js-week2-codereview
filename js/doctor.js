a30181ac3f27cb512c4546a948d999ca3953374c
Kimberly Bordon <kbordon@gmail.com>
2017-12-01 12:18:55 -0800

    create promise for user query

--- js/doctor.js
+++ js/doctor.js
@@ -18,26 +18,35 @@ export class ApplicationModule{
     console.log(apiKey);
   }
 
-  getDoctors(userInputQuery = "", userInputName = "", displayData) {
-    let query = `${userInputQuery}&`;
-    if (userInputQuery === null) {
-      query = "";
-    }
-    let drName = `${userInputQuery}&`;
-
-
+  getDoctors(userQuery = "", userDr = "", displayData) {
     let promiseDrs = new Promise(function(resolve, reject){
       let requestDrs = new XMLHttpRequest();
-      let urlDrs = ` https://api.betterdoctor.com/2016-03-01/doctors?${drName}location=or-portland&sort=best-match-desc&skip=0&limit=10&user_key=${apiKey}`
-    })
+      let urlDrs = `https://api.betterdoctor.com/2016-03-01/doctors?name=${userDr}&query=${userQuery}&location=or-portland&sort=best-match-desc&skip=0&limit=10&user_key=${apiKey}`;
+      requestDrs.onload = function(response) {
+        if (this.status === 200) {
+          resolve(requestDrs.response);
+        } else {
+          reject(Error(requestDrs.response.meta.message));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
 
+    promiseDrs.then(function(response){
+      body = JSON.parse(response);
+      displayData(response);
+    }, function(error) {
+      displayErrors(error);
+    });
 
-    $.get(`` + userInput + apiKey)
-      .then(function(results) {
-        displayData(results);
-      })
-      .fail(function() {
-        console.log(`Something went wrong.`);
-      });
+    // tutorial for separating logic uses ajax
+    // $.get(`` + userInput + apiKey)
+    //   .then(function(results) {
+    //     displayData(results);
+    //   })
+    //   .fail(function() {
+    //     console.log(`Something went wrong.`);
+    //   });
   }
 }
